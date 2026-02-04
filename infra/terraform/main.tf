a43527d50c6dacfd024f0a62c9875e9fe206ec71
Sandaru-Lakmika <lakmikagamage@gmail.com>
2026-02-04 13:30:26 +0530

    Remove unused AWS EC2 instance resource configuration from Terraform

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -26,18 +26,3 @@ provider "aws" {
 data "aws_instance" "existing_instance" {
   instance_id = "i-0d8e67243526a67b3" // Replace with the actual instance ID
 }
-
-resource "aws_instance" "app_server" {
-  ami                                  = data.aws_instance.existing_instance.ami
-  instance_type                        = data.aws_instance.existing_instance.instance_type
-  key_name                             = data.aws_instance.existing_instance.key_name
-  subnet_id                            = data.aws_instance.existing_instance.subnet_id
-  vpc_security_group_ids               = data.aws_instance.existing_instance.vpc_security_group_ids
-
-  tags = {
-    Name        = "my-docker-app2-server"
-    Environment = "production"
-    ManagedBy   = "Terraform"
-    Project     = "my-docker-app2"
-  }
-}
